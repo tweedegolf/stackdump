e64b644797b7ff86a5950564893477f846bfa2ea
Dion Dokter <diondokter@gmail.com>
2022-06-28 14:53:33 +0200

    Improved publish script

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,9 @@
 cargo publish -p stackdump-core
+Start-Sleep -Seconds 5
 cargo publish -p stackdump-capture
+Start-Sleep -Seconds 5
 cargo publish -p stackdump-trace
+Start-Sleep -Seconds 5
 cargo publish -p stackdump-capture-probe
+Start-Sleep -Seconds 5
+cargo publish -p stackdump-cli
