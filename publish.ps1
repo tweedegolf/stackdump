421ff01d17c345a54e396ab2d992bc642585c963
Dion Dokter <diondokter@gmail.com>
2023-04-07 09:37:44 +0200

    Remove sleep that is no longer required

--- publish.ps1
+++ publish.ps1
@@ -1,9 +1,5 @@
 cargo publish -p stackdump-core
-Start-Sleep -Seconds 5
 cargo publish -p stackdump-capture
-Start-Sleep -Seconds 5
 cargo publish -p stackdump-trace
-Start-Sleep -Seconds 5
 cargo publish -p stackdump-capture-probe
-Start-Sleep -Seconds 5
 cargo publish -p stackdump-cli
